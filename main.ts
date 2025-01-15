1cf7d4c6221d46161684bcd8aa86a956d8e1d72a
razoalv2107 <razoalv2107@g.educaand.es>
2025-01-15 12:18:24 +0100

    corregido el empezar la cuenta por cero

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    contador = 0
+    contador = -1
 })
 let contador = 0
-contador = 0
+contador = -1
 basic.forever(function () {
-    basic.showNumber(contador)
     contador += 1
-    basic.pause(1000)
+    basic.showNumber(contador)
     if (contador > 20) {
-        contador = 0
+        contador = -1
     }
+    basic.pause(1000)
 })
